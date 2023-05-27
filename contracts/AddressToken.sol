c5393364fb0ae9f7ccacb49ae114028d8b53a069
Anton Bukov <k06aaa@gmail.com>
2023-05-27 14:31:34 +0200

    Fixes and improvements

--- contracts/AddressToken.sol
+++ contracts/AddressToken.sol
@@ -7,28 +7,112 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "solmate/src/utils/CREATE3.sol";
 
 contract AddressToken is ERC721("1inch Address NFT", "1ANFT") {
-    error RemintForbidden();
     error AccessDenied();
-    error CallReverted(bytes);
+    error RemintForbidden();
+    error CallReverted(uint256, bytes);
 
-    mapping(uint256 tokenId => bytes32 salt) public salts;
+    mapping(address tokenId => bytes32 salt) public salts;
 
     function addressForTokenId(uint256 tokenId) external pure returns(address) {
         return address(uint160(tokenId));
     }
 
     function tokenURI(uint256 tokenId) public pure override returns(string memory) {
-        bytes memory addr = bytes(Strings.toHexString(tokenId, 20));
-        _checksumAddress(addr);
-        return string.concat("data:application/json;base64,", Base64.encode(
-            bytes.concat('{\n',
-                '\t"name": "Deploy to ', addr, '",\n',
-                '\t"description": "Enables holder to deploy arbitrary smart contract to ', addr, '",\n',
-                '\t"external_url": "https://etherscan.io/address/', addr, '",\n',
-                '\t"image": "ipfs://QmZW3TTdtK87ktxmh6PG5UumbtoWXU8rVBApo65oknekmc",\n',
-                '\t"animation_url": "ipfs://QmZKp3K7oyDFPkVUXUgDKqZ6RcLZY7QW267JvXRTLW1qaG"\n'
-            '}')
-        ));
+        return string.concat("data:application/json;base64,", Base64.encode(bytes(tokenJSON(tokenId))));
+    }
+
+    function tokenJSON(uint256 tokenId) public pure returns(string memory) {
+        bytes memory accountHex = bytes(Strings.toHexString(tokenId, 20));
+        bytes memory attributes = _repeatedAttributes('', accountHex);
+        attributes = _wordsAttributes(attributes, accountHex);
+        if (attributes.length > 0) {
+            attributes = bytes.concat(attributes, '\n');
+        }
+
+        _checksumAddress(accountHex);
+
+        bytes memory json = bytes.concat('{\n',
+            '\t"name": "Deploy to ', accountHex, '",\n',
+            '\t"description": "Enables holder to deploy arbitrary smart contract to ', accountHex, '",\n',
+            '\t"external_url": "https://etherscan.io/address/', accountHex, '",\n',
+            '\t"image": "ipfs://QmZW3TTdtK87ktxmh6PG5UumbtoWXU8rVBApo65oknekmc",\n',
+            '\t"animation_url": "ipfs://QmZKp3K7oyDFPkVUXUgDKqZ6RcLZY7QW267JvXRTLW1qaG",\n',
+            (attributes.length > 0
+                ? bytes.concat('\t"attributes": [\n', attributes, '\t]\n')
+                : bytes.concat('')),
+        '}');
+
+        return string(json);
+    }
+
+    function _repeatedAttributes(bytes memory attributes, bytes memory accountHex) private pure returns(bytes memory) {
+        uint256 length = 1;
+        bytes1 letter = accountHex[2];
+        for (uint256 j = 3; j < 42; j++) {
+            if (accountHex[j] == letter) {
+                length++;
+            }
+
+            if (accountHex[j] != letter || j == 41) {
+                if (length >= 4) {
+                    if (attributes.length > 0) {
+                        attributes = bytes.concat(attributes, ',\n');
+                    }
+                    if (length + 2 == j) {
+                        attributes = bytes.concat(attributes, '\t\t{\n\t\t\t"trait_type": "Repeated prefix ', letter, '",\n\t\t\t"value": ', bytes(Strings.toString(length)), '\n\t\t}');
+                    }
+                    else if (j == 41) {
+                        attributes = bytes.concat(attributes, '\t\t{\n\t\t\t"trait_type": "Repeated suffix ', letter, '",\n\t\t\t"value": ', bytes(Strings.toString(length)), '\n\t\t}');
+                    }
+                    else {
+                        attributes = bytes.concat(attributes, '\t\t{\n\t\t\t"trait_type": "Repeated symbols ', letter, '",\n\t\t\t"value": ', bytes(Strings.toString(length)), '\n\t\t}');
+                    }
+                }
+                length = 1;
+                letter = accountHex[j];
+            }
+        }
+
+        return attributes;
+    }
+
+    function _wordsAttributes(bytes memory attributes, bytes memory accountHex) private pure returns(bytes memory) {
+        attributes = _wordAttributes(attributes, accountHex, 'dead');
+        attributes = _wordAttributes(attributes, accountHex, 'beef');
+        attributes = _wordAttributes(attributes, accountHex, 'c0ffee');
+        attributes = _wordAttributes(attributes, accountHex, 'def1');
+        attributes = _wordAttributes(attributes, accountHex, '1ee7');
+        return attributes;
+    }
+
+    function _wordAttributes(bytes memory attributes, bytes memory accountHex, bytes memory word) private pure returns(bytes memory) {
+        // Look for word in account
+        uint256 found = 0;
+        for (uint256 i = 2; i < 42; i++) {
+            uint256 matched = 0;
+            for (uint256 j = 0; j < word.length && i + j < 42; j++) {
+                if (accountHex[i + j] == word[j]) {
+                    matched++;
+                }
+                else {
+                    break;
+                }
+            }
+
+            if (matched == word.length) {
+                found++;
+                i += word.length - 1;
+            }
+        }
+
+        if (found > 0) {
+            if (attributes.length > 0) {
+                attributes = bytes.concat(attributes, ',\n');
+            }
+            attributes = bytes.concat(attributes, '\t\t{\n\t\t\t"trait_type": "Contains ', word, '",\n\t\t\t"value": ', bytes(Strings.toString(found)), '\n\t\t}');
+        }
+
+        return attributes;
     }
 
     function _checksumAddress(bytes memory hexAddress) private pure {
@@ -44,28 +128,34 @@ contract AddressToken is ERC721("1inch Address NFT", "1ANFT") {
         }
     }
 
-    function addressAndSaltForMagic(bytes16 magic) public view returns(address account, bytes32 salt) {
-        salt = bytes32(uint256(type(uint128).max & uint160(msg.sender))) | bytes32(magic);
-        account = CREATE3.getDeployed(salt);
+    function addressAndSaltForMagic(bytes16 magic, address account) public view returns(address tokenId, bytes32 salt) {
+        salt = bytes32(uint256(type(uint128).max & uint160(account))) | bytes32(magic);
+        tokenId = CREATE3.getDeployed(salt);
     }
 
-    function mint(bytes16 magic) external returns(uint256 tokenId) {
-        (address account, bytes32 salt) = addressAndSaltForMagic(magic);
-        tokenId = uint160(account);
+    function mint(bytes16 magic) external returns(address tokenId) {
+        return mintFor(magic, msg.sender);
+    }
+
+    function mintFor(bytes16 magic, address account) public returns(address tokenId) {
+        bytes32 salt;
+        (tokenId, salt) = addressAndSaltForMagic(magic, account);
         if (salts[tokenId] != 0) revert RemintForbidden();
         salts[tokenId] = salt;
-        _mint(msg.sender, tokenId);
+        _mint(account, uint160(tokenId));
     }
 
-    function deploy(uint256 tokenId, bytes calldata creationCode) public payable returns(address deployed) {
-        if (msg.sender != ownerOf(tokenId)) revert AccessDenied();
-        _burn(tokenId);
+    function deploy(address tokenId, bytes calldata creationCode) public payable returns(address deployed) {
+        if (msg.sender != ownerOf(uint160(tokenId))) revert AccessDenied();
+        _burn(uint160(tokenId));
         deployed = CREATE3.deploy(salts[tokenId], creationCode, msg.value);
     }
 
-    function deployAndCall(uint256 tokenId, bytes calldata creationCode, bytes calldata cd) external payable returns(address deployed) {
+    function deployAndCalls(address tokenId, bytes calldata creationCode, bytes[] calldata cds) external payable returns(address deployed) {
         deployed = deploy(tokenId, creationCode);
-        (bool success, bytes memory reason) = deployed.call(cd);
-        if (!success) revert CallReverted(reason);
+        for (uint256 i = 0; i < cds.length; i++) {
+            (bool success, bytes memory reason) = deployed.call(cds[i]);
+            if (!success) revert CallReverted(i, reason);
+        }
     }
 }
--- test/AddressToken.js
+++ test/AddressToken.js
@@ -3,23 +3,182 @@ const { ethers } = hre;
 const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
 const { expect } = require('chai');
 const { trim0x } = require('@1inch/solidity-utils');
+const { BigNumber } = require('ethers');
 
 describe('AddressToken', async function () {
     async function initContracts () {
         const AddressToken = await ethers.getContractFactory('AddressToken');
         const addressToken = await AddressToken.deploy();
         await addressToken.deployed();
-        const [ signer ] = await ethers.getSigners();
+        const [signer] = await ethers.getSigners();
+        console.log('signer', signer.address);
+        console.log('addressToken', addressToken.address);
         return { signer, addressToken };
     }
 
-    it('should be ok', async function () {
-        const { signer, addressToken } = await loadFixture(initContracts);
-        
-        const magic = '0x11223344556677881122334455667788';
-        const tokenId = await addressToken.callStatic.mint(magic);
-        await addressToken.mint(magic);
-        expect(await addressToken.salts(tokenId)).to.be.equal(magic + signer.address.substring(10).toLowerCase());
-        expect(await addressToken.tokenURI(tokenId)).to.be.equal('data:;base64,ewoJImRlc2NyaXB0aW9uIjogIkFsbG93cyBkZXBsb3lpbmcgdG8gMHg3ZjZDMTM3RGMwN2ZFNUU3MjM0OGEzNmRiN0M1MjYxMjNEM2JCN2UxIiwKCSJuYW1lIjogIkFkZHJlc3MgMHg3ZjZDMTM3RGMwN2ZFNUU3MjM0OGEzNmRiN0M1MjYxMjNEM2JCN2UxIgoJImV4dGVybmFsX3VybCI6ICJodHRwczovL2V0aGVyc2Nhbi5pby9hZGRyZXNzLzB4N2Y2QzEzN0RjMDdmRTVFNzIzNDhhMzZkYjdDNTI2MTIzRDNiQjdlMSIKfQ==');
+    describe('mint and tokenURI', async function () {
+        it('should work for 0x000000C13168BcEfeA11dE78747e4D314ea40838', async function () {
+            const { signer, addressToken } = await loadFixture(initContracts);
+
+            const magic = '0x0559d2424d880100b1a2090000000000';
+            const { tokenId } = await addressToken.addressAndSaltForMagic(magic, signer.address);
+            expect(tokenId).to.be.equal('0x000000C13168BcEfeA11dE78747e4D314ea40838');
+            await addressToken.mint(magic);
+
+            const tokenURI = await addressToken.tokenURI(tokenId);
+            expect(JSON.parse(atob(tokenURI.substring(29)))).to.be.deep.equal(
+                {
+                    name: 'Deploy to 0x000000C13168BcEfeA11dE78747e4D314ea40838',
+                    description: 'Enables holder to deploy arbitrary smart contract to 0x000000C13168BcEfeA11dE78747e4D314ea40838',
+                    external_url: 'https://etherscan.io/address/0x000000C13168BcEfeA11dE78747e4D314ea40838',
+                    image: 'ipfs://QmZW3TTdtK87ktxmh6PG5UumbtoWXU8rVBApo65oknekmc',
+                    animation_url: 'ipfs://QmZKp3K7oyDFPkVUXUgDKqZ6RcLZY7QW267JvXRTLW1qaG',
+                    attributes: [
+                        { trait_type: 'Repeated prefix 0', value: 6 },
+                    ],
+                },
+            );
+        });
+
+        it('should work for 0x52A4937394Cb46Cda07F6CC68078a95900000000', async function () {
+            const { signer, addressToken } = await loadFixture(initContracts);
+
+            const magic = '0xcd0fe5e77823fccdc05ea4efaf652692';
+            const { tokenId } = await addressToken.addressAndSaltForMagic(magic, signer.address);
+            expect(tokenId).to.be.equal('0x52A4937394Cb46Cda07F6CC68078a95900000000');
+            await addressToken.mint(magic);
+
+            const tokenURI = await addressToken.tokenURI(tokenId);
+            expect(JSON.parse(atob(tokenURI.substring(29)))).to.be.deep.equal(
+                {
+                    name: 'Deploy to 0x52A4937394Cb46Cda07F6CC68078a95900000000',
+                    description: 'Enables holder to deploy arbitrary smart contract to 0x52A4937394Cb46Cda07F6CC68078a95900000000',
+                    external_url: 'https://etherscan.io/address/0x52A4937394Cb46Cda07F6CC68078a95900000000',
+                    image: 'ipfs://QmZW3TTdtK87ktxmh6PG5UumbtoWXU8rVBApo65oknekmc',
+                    animation_url: 'ipfs://QmZKp3K7oyDFPkVUXUgDKqZ6RcLZY7QW267JvXRTLW1qaG',
+                    attributes: [
+                        { trait_type: 'Repeated suffix 0', value: 8 },
+                    ],
+                },
+            );
+        });
+
+        it('should work for 0x0000111100B1333Fd60c33A0f5eC3a1D21b40eF5', async function () {
+            const { signer, addressToken } = await loadFixture(initContracts);
+
+            const magic = '0x02b101bc528801005194680000000000';
+            const { tokenId } = await addressToken.addressAndSaltForMagic(magic, signer.address);
+            expect(tokenId).to.be.equal('0x0000111100B1333Fd60c33A0f5eC3a1D21b40eF5');
+            await addressToken.mint(magic);
+
+            const tokenURI = await addressToken.tokenURI(tokenId);
+            expect(JSON.parse(atob(tokenURI.substring(29)))).to.be.deep.equal(
+                {
+                    name: 'Deploy to 0x0000111100B1333Fd60c33A0f5eC3a1D21b40eF5',
+                    description: 'Enables holder to deploy arbitrary smart contract to 0x0000111100B1333Fd60c33A0f5eC3a1D21b40eF5',
+                    external_url: 'https://etherscan.io/address/0x0000111100B1333Fd60c33A0f5eC3a1D21b40eF5',
+                    image: 'ipfs://QmZW3TTdtK87ktxmh6PG5UumbtoWXU8rVBApo65oknekmc',
+                    animation_url: 'ipfs://QmZKp3K7oyDFPkVUXUgDKqZ6RcLZY7QW267JvXRTLW1qaG',
+                    attributes: [
+                        { trait_type: 'Repeated prefix 0', value: 4 },
+                        { trait_type: 'Repeated symbols 1', value: 4 },
+                    ],
+                },
+            );
+        });
+
+        it('should work for 0x00001000072D7AA9191B731Aa2346286e6E78FB9', async function () {
+            const { signer, addressToken } = await loadFixture(initContracts);
+
+            const magic = '0x031ddf145388010016fab98202000000';
+            const { tokenId } = await addressToken.addressAndSaltForMagic(magic, signer.address);
+            expect(tokenId).to.be.equal('0x00001000072D7AA9191B731Aa2346286e6E78FB9');
+            await addressToken.mint(magic);
+
+            const tokenURI = await addressToken.tokenURI(tokenId);
+            expect(JSON.parse(atob(tokenURI.substring(29)))).to.be.deep.equal(
+                {
+                    name: 'Deploy to 0x00001000072D7AA9191B731Aa2346286e6E78FB9',
+                    description: 'Enables holder to deploy arbitrary smart contract to 0x00001000072D7AA9191B731Aa2346286e6E78FB9',
+                    external_url: 'https://etherscan.io/address/0x00001000072D7AA9191B731Aa2346286e6E78FB9',
+                    image: 'ipfs://QmZW3TTdtK87ktxmh6PG5UumbtoWXU8rVBApo65oknekmc',
+                    animation_url: 'ipfs://QmZKp3K7oyDFPkVUXUgDKqZ6RcLZY7QW267JvXRTLW1qaG',
+                    attributes: [
+                        { trait_type: 'Repeated prefix 0', value: 4 },
+                        { trait_type: 'Repeated symbols 0', value: 4 },
+                    ],
+                },
+            );
+        });
+
+        it('should work for 0x000000087b51e4ABc32c8450fe6eF726707EeB8d', async function () {
+            const { signer, addressToken } = await loadFixture(initContracts);
+
+            const magic = '0xb065c0eb80adac2bf3112f7724a5a523';
+            const { tokenId } = await addressToken.addressAndSaltForMagic(magic, signer.address);
+            expect(tokenId).to.be.equal('0x000000087b51e4ABc32c8450fe6eF726707EeB8d');
+            await addressToken.mint(magic);
+
+            const tokenURI = await addressToken.tokenURI(tokenId);
+            expect(JSON.parse(atob(tokenURI.substring(29)))).to.be.deep.equal(
+                {
+                    name: 'Deploy to 0x000000087b51e4ABc32c8450fe6eF726707EeB8d',
+                    description: 'Enables holder to deploy arbitrary smart contract to 0x000000087b51e4ABc32c8450fe6eF726707EeB8d',
+                    external_url: 'https://etherscan.io/address/0x000000087b51e4ABc32c8450fe6eF726707EeB8d',
+                    image: 'ipfs://QmZW3TTdtK87ktxmh6PG5UumbtoWXU8rVBApo65oknekmc',
+                    animation_url: 'ipfs://QmZKp3K7oyDFPkVUXUgDKqZ6RcLZY7QW267JvXRTLW1qaG',
+                    attributes: [
+                        { trait_type: 'Repeated prefix 0', value: 7 },
+                    ],
+                },
+            );
+        });
+
+        it('should work for 0xDeadbEefE12311Cb7FDb1f7Bc8cf6fe9904E7Bdb', async function () {
+            const { signer, addressToken } = await loadFixture(initContracts);
+
+            const magic = '0x137652d237f63d049d954e0ce97b5976';
+            const { tokenId } = await addressToken.addressAndSaltForMagic(magic, signer.address);
+            expect(tokenId).to.be.equal('0xDeadbEefE12311Cb7FDb1f7Bc8cf6fe9904E7Bdb');
+            await addressToken.mint(magic);
+
+            const tokenURI = await addressToken.tokenURI(tokenId);
+            expect(JSON.parse(atob(tokenURI.substring(29)))).to.be.deep.equal(
+                {
+                    name: 'Deploy to 0xDeadbEefE12311Cb7FDb1f7Bc8cf6fe9904E7Bdb',
+                    description: 'Enables holder to deploy arbitrary smart contract to 0xDeadbEefE12311Cb7FDb1f7Bc8cf6fe9904E7Bdb',
+                    external_url: 'https://etherscan.io/address/0xDeadbEefE12311Cb7FDb1f7Bc8cf6fe9904E7Bdb',
+                    image: 'ipfs://QmZW3TTdtK87ktxmh6PG5UumbtoWXU8rVBApo65oknekmc',
+                    animation_url: 'ipfs://QmZKp3K7oyDFPkVUXUgDKqZ6RcLZY7QW267JvXRTLW1qaG',
+                    attributes: [
+                        { trait_type: 'Contains dead', value: 1 },
+                        { trait_type: 'Contains beef', value: 1 },
+                    ],
+                },
+            );
+        });
+
+        it('should work for 0xdeF1def1083CA3aB7F8296247E0c6C4A4d88bB0E', async function () {
+            const { signer, addressToken } = await loadFixture(initContracts);
+
+            const magic = '0x4057ad8e6a801a25b9a97d2a0ae5ebbc';
+            const { tokenId } = await addressToken.addressAndSaltForMagic(magic, signer.address);
+            expect(tokenId).to.be.equal('0xdeF1def1083CA3aB7F8296247E0c6C4A4d88bB0E');
+            await addressToken.mint(magic);
+
+            const tokenURI = await addressToken.tokenURI(tokenId);
+            expect(JSON.parse(atob(tokenURI.substring(29)))).to.be.deep.equal(
+                {
+                    name: 'Deploy to 0xdeF1def1083CA3aB7F8296247E0c6C4A4d88bB0E',
+                    description: 'Enables holder to deploy arbitrary smart contract to 0xdeF1def1083CA3aB7F8296247E0c6C4A4d88bB0E',
+                    external_url: 'https://etherscan.io/address/0xdeF1def1083CA3aB7F8296247E0c6C4A4d88bB0E',
+                    image: 'ipfs://QmZW3TTdtK87ktxmh6PG5UumbtoWXU8rVBApo65oknekmc',
+                    animation_url: 'ipfs://QmZKp3K7oyDFPkVUXUgDKqZ6RcLZY7QW267JvXRTLW1qaG',
+                    attributes: [
+                        { trait_type: 'Contains def1', value: 2 },
+                    ],
+                },
+            );
+        });
     });
 });
